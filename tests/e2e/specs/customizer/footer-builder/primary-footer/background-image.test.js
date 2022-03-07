592cb2d7488459102d53c4c5a38de705b2fb8529
bsf-simranc <87968514+SimranChougule1@users.noreply.github.com>
2022-03-07 14:11:41 +0530

    Update background-image.test.js

--- tests/e2e/specs/customizer/footer-builder/primary-footer/background-image.test.js
+++ tests/e2e/specs/customizer/footer-builder/primary-footer/background-image.test.js
@@ -2,13 +2,12 @@ import { createURL } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
 import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 import { scrollToElement } from '../../../../utils/scroll-to-element';
-describe( 'primary footer backgeround image setting in customizer', () => {
+describe( 'Primary footer backgeround image setting in customizer', () => {
 	it( 'background image for desktop should apply correctly', async () => {
 		const primaryFooter = {
 			'hb-footer-bg-obj-responsive': {
 				desktop: {
-					'background-image': 'https://images.ctfassets.net/hrltx12pl8hq/4CH9hYoMSORDuL8BXgX0Br/c2520603e3d2616468161c38519c9446/shutterstock_631575830.jpg?fit=fill&w=2560&h=400&fm=webp',
-
+					'background-image': 'https://pxhere.com/en/photo/613364',
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
 					'background-size': 'cover',
@@ -16,8 +15,7 @@ describe( 'primary footer backgeround image setting in customizer', () => {
 					'background-type': 'image',
 				},
 				tablet: {
-					'background-image': 'https://images.ctfassets.net/hrltx12pl8hq/4CH9hYoMSORDuL8BXgX0Br/c2520603e3d2616468161c38519c9446/shutterstock_631575830.jpg?fit=fill&w=2560&h=400&fm=webp',
-
+					'background-image': 'https://pxhere.com/en/photo/707755',
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
 					'background-size': 'cover',
@@ -25,8 +23,7 @@ describe( 'primary footer backgeround image setting in customizer', () => {
 					'background-type': 'image',
 				},
 				mobile: {
-					'background-image': 'https://images.ctfassets.net/hrltx12pl8hq/4CH9hYoMSORDuL8BXgX0Br/c2520603e3d2616468161c38519c9446/shutterstock_631575830.jpg?fit=fill&w=2560&h=400&fm=webp',
-
+					'background-image': 'https://pxhere.com/en/photo/5204',
 					'background-repeat': 'no-repeat',
 					'background-position': 'left top',
 					'background-size': 'cover',
@@ -52,24 +49,18 @@ describe( 'primary footer backgeround image setting in customizer', () => {
 		await expect( {
 			selector: '.site-primary-footer-wrap[data-section="section-primary-footer-builder"]',
 			property: 'background-image',
-		} ).cssValueToBe(
-			`url("${ primaryFooter[ 'hb-footer-bg-obj-responsive' ].desktop[ 'background-image' ] + '")' }`,
-		);
+		} ).cssValueToBe( `url("${ primaryFooter[ 'hb-footer-bg-obj-responsive' ].desktop[ 'background-image' ] + '")' }` );
 		await setBrowserViewport( 'medium' );
 		await scrollToElement( '#colophon' );
 		await expect( {
 			selector: '.site-primary-footer-wrap[data-section="section-primary-footer-builder"]',
 			property: 'background-image',
-		} ).cssValueToBe(
-			`url("${ primaryFooter[ 'hb-footer-bg-obj-responsive' ].tablet[ 'background-image' ] + '")' }`,
-		);
+		} ).cssValueToBe( `url("${ primaryFooter[ 'hb-footer-bg-obj-responsive' ].tablet[ 'background-image' ] + '")' }` );
 		await setBrowserViewport( 'small' );
 		await scrollToElement( '#colophon' );
 		await expect( {
 			selector: '.site-primary-footer-wrap[data-section="section-primary-footer-builder"]',
 			property: 'background-image',
-		} ).cssValueToBe(
-			`url("${ primaryFooter[ 'hb-footer-bg-obj-responsive' ].mobile[ 'background-image' ] + '")' }`,
-		);
+		} ).cssValueToBe( `url("${ primaryFooter[ 'hb-footer-bg-obj-responsive' ].mobile[ 'background-image' ] + '")' }` );
 	} );
 } );
