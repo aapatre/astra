395f6c14cf50ab44e049999ec929c94f5b3de639
Simran <simranc@bsf.io>
2022-03-07 11:53:29 +0530

    modified code

--- tests/e2e/specs/customizer/footer-builder/primary-footer/background-color.test.js
+++ tests/e2e/specs/customizer/footer-builder/primary-footer/background-color.test.js
@@ -2,19 +2,18 @@ import { createURL } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
 import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 import { scrollToElement } from '../../../../utils/scroll-to-element';
-
-describe( 'primary footer background color settings in the customizer', () => {
+describe( 'Primary footer background color settings in the customizer', () => {
 	it( 'background color should apply correctly', async () => {
-		const backgroundcolor = {
+		const backgroundColor = {
 			'hb-footer-bg-obj-responsive': {
 				desktop: {
-					'background-color': 'rgb(210, 248, 249)',
+					'background-color': 'rgb(224, 218, 185)',
 				},
 				tablet: {
-					'background-color': 'rgb(210, 248, 249)',
+					'background-color': 'rgb(194, 237, 239)',
 				},
 				mobile: {
-					'background-color': 'rgb(210, 248, 249)',
+					'background-color': 'rgb(225, 197, 232)',
 				},
 			},
 			'footer-desktop-items': {
@@ -25,33 +24,30 @@ describe( 'primary footer background color settings in the customizer', () => {
 				},
 			},
 		};
-		await setCustomize( backgroundcolor );
+		await setCustomize( backgroundColor );
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '.site-primary-footer-wrap[data-section="section-primary-footer-builder"]' );
 		await setBrowserViewport( 'large' );
 		await scrollToElement( '#colophon' );
-
+		await page.waitForSelector( '.site-primary-footer-wrap[data-section="section-primary-footer-builder"]' );
 		await expect( {
 			selector: '.site-primary-footer-wrap[data-section="section-primary-footer-builder"]',
 			property: 'background-color',
-		} ).cssValueToBe( `${ backgroundcolor[ 'hb-footer-bg-obj-responsive' ].desktop[ 'background-color' ] }` );
+		} ).cssValueToBe( `${ backgroundColor[ 'hb-footer-bg-obj-responsive' ].desktop[ 'background-color' ] }` );
 
-		await page.waitForSelector( '.site-primary-footer-wrap[data-section="section-primary-footer-builder"]' );
 		await setBrowserViewport( 'medium' );
 		await scrollToElement( '#colophon' );
 		await expect( {
 			selector: '.site-primary-footer-wrap[data-section="section-primary-footer-builder"]',
 			property: 'background-color',
-		} ).cssValueToBe( `${ backgroundcolor[ 'hb-footer-bg-obj-responsive' ].tablet[ 'background-color' ] }` );
+		} ).cssValueToBe( `${ backgroundColor[ 'hb-footer-bg-obj-responsive' ].tablet[ 'background-color' ] }` );
 
-		await page.waitForSelector( '.site-primary-footer-wrap[data-section="section-primary-footer-builder"]' );
 		await setBrowserViewport( 'small' );
 		await scrollToElement( '#colophon' );
 		await expect( {
 			selector: '.site-primary-footer-wrap[data-section="section-primary-footer-builder"]',
 			property: 'background-color',
-		} ).cssValueToBe( `${ backgroundcolor[ 'hb-footer-bg-obj-responsive' ].mobile[ 'background-color' ] }` );
+		} ).cssValueToBe( `${ backgroundColor[ 'hb-footer-bg-obj-responsive' ].mobile[ 'background-color' ] }` );
 	} );
 } );
