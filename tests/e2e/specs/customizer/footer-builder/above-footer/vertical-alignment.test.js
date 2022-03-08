12cfe12c689ceec325ea3a4f62379c0a46ada6fa
Simran <simranc@bsf.io>
2022-03-08 09:36:16 +0530

    modified code

--- tests/e2e/specs/customizer/footer-builder/above-footer/vertical-alignment.test.js
+++ tests/e2e/specs/customizer/footer-builder/above-footer/vertical-alignment.test.js
@@ -3,88 +3,32 @@ import { setCustomize } from '../../../../utils/customize';
 import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 import { scrollToElement } from '../../../../utils/scroll-to-element';
 describe( 'Above footer vertical alignment setting in customizer', () => {
-	it( 'verical alignment top should apply correctly', async () => {
+	it( 'verical alignment should apply correctly', async () => {
 		const verticalAlignment = {
-			'hba-footer-vertical-alignment': 'flex-start',
+			'footer-copyright-editor': 'Copyright [copyright] [current_year] [site_title] | Powered by [theme_author] Copyright [copyright] [current_year] [site_title] | Powered by [theme_author]',
+			'hba-footer-column': 3,
+			'hba-footer-vertical-alignment': 'flex-end',
 			'footer-desktop-items': {
 				above: {
 					above_1: {
 						0: 'social-icons-1',
 					},
+					above_2: {
+						0: 'copyright',
+					},
 				},
 			},
 		};
 		await setCustomize( verticalAlignment );
-
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
 		await setBrowserViewport( 'large' );
-
 		await scrollToElement( '#colophon' );
-
 		await page.waitForSelector( '.site-above-footer-wrap[data-section="section-above-footer-builder"] .ast-builder-grid-row' );
 		await expect( {
 			selector: '.site-above-footer-wrap[data-section="section-above-footer-builder"] .ast-builder-grid-row',
 			property: 'align-items',
-		} ).cssValueToBe( `${ verticalAlignment[ 'hba-footer-vertical-alignment' ] }`,
-
-		);
-	} );
-	it( 'verical alignment center should apply correctly', async () => {
-		const verticalAlignment = {
-			'hbb-footer-vertical-alignment': 'center',
-			'footer-desktop-items': {
-				above: {
-					above_1: {
-						0: 'social-icons-1',
-					},
-				},
-			},
-		};
-		await setCustomize( verticalAlignment );
-
-		await page.goto( createURL( '/' ), {
-			waitUntil: 'networkidle0',
-		} );
-		await setBrowserViewport( 'medium' );
-
-		await scrollToElement( '#colophon' );
-
-		await page.waitForSelector( '.site-above-footer-wrap[data-section="section-above-footer-builder"] .ast-builder-grid-row' );
-		await expect( {
-			selector: '.site-above-footer-wrap[data-section="section-above-footer-builder"] .ast-builder-grid-row',
-			property: 'align-items',
-		} ).cssValueToBe( `${ verticalAlignment[ 'hba-footer-vertical-alignment' ] }`,
-
-		);
-	} );
-	it( 'verical alignment bottom should apply correctly', async () => {
-		const verticalAlignment = {
-			'hbb-footer-vertical-alignment': 'flex-end',
-			'footer-desktop-items': {
-				above: {
-					above_1: {
-						0: 'social-icons-1',
-					},
-				},
-			},
-		};
-		await setCustomize( verticalAlignment );
-
-		await page.goto( createURL( '/' ), {
-			waitUntil: 'networkidle0',
-		} );
-		await setBrowserViewport( 'medium' );
-
-		await scrollToElement( '#colophon' );
-
-		await page.waitForSelector( '.site-above-footer-wrap[data-section="section-above-footer-builder"] .ast-builder-grid-row' );
-		await expect( {
-			selector: '.site-above-footer-wrap[data-section="section-above-footer-builder"] .ast-builder-grid-row',
-			property: 'align-items',
-		} ).cssValueToBe( `${ verticalAlignment[ 'hba-footer-vertical-alignment' ] }`,
-
-		);
+		} ).cssValueToBe( `${ verticalAlignment[ 'hba-footer-vertical-alignment' ] }` );
 	} );
 } );
