220d7f3f95e86063b1e1ff3f75a7273a95602087
Simran <simranc@bsf.io>
2022-03-08 09:20:48 +0530

    modified code

--- tests/e2e/specs/customizer/footer-builder/above-footer/inner-column-spacing.test.js
+++ tests/e2e/specs/customizer/footer-builder/above-footer/inner-column-spacing.test.js
@@ -2,14 +2,15 @@ import { createURL } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../../utils/customize';
 import { setBrowserViewport } from '../../../../utils/set-browser-viewport';
 import { scrollToElement } from '../../../../utils/scroll-to-element';
-describe( 'Primary footer inner column spacing setting in customizer', () => {
+describe( 'Above footer inner column spacing setting in customizer', () => {
 	it( 'inner column spacing should apply correctly', async () => {
-		const innercolumnSpacing = {
+		const innerColumnSpacing = {
+			'hba-footer-column': 2,
 			'hba-inner-spacing':
 			{
-				desktop: '50',
-				tablet: '50',
-				mobile: '50',
+				desktop: '90',
+				tablet: '70',
+				mobile: '40',
 				'desktop-unit': 'px',
 				'tablet-unit': 'px',
 				'mobile-unit': 'px',
@@ -19,38 +20,36 @@ describe( 'Primary footer inner column spacing setting in customizer', () => {
 					above_1: {
 						0: 'social-icons-1',
 					},
+					above_2: {
+						0: 'copyright',
+					},
 				},
 			},
 		};
-
-		await setCustomize( innercolumnSpacing );
+		await setCustomize( innerColumnSpacing );
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '.site-above-footer-wrap[data-section="section-above-footer-builder"] .ast-builder-grid-row' );
 		await setBrowserViewport( 'large' );
 		await scrollToElement( '#colophon' );
-
+		await page.waitForSelector( '.site-above-footer-wrap[data-section="section-above-footer-builder"] .ast-builder-grid-row' );
 		await expect( {
 			selector: '.site-above-footer-wrap[data-section="section-above-footer-builder"] .ast-builder-grid-row',
 			property: 'grid-column-gap',
-		} ).cssValueToBe( `${ innercolumnSpacing[ 'hba-inner-spacing' ].desktop }${ innercolumnSpacing[ 'hba-inner-spacing' ][ 'desktop-unit' ] }`,
-		);
+		} ).cssValueToBe( `${ innerColumnSpacing[ 'hba-inner-spacing' ].desktop }${ innerColumnSpacing[ 'hba-inner-spacing' ][ 'desktop-unit' ] }` );
 
 		await setBrowserViewport( 'medium' );
 		await scrollToElement( '#colophon' );
 		await expect( {
 			selector: '.site-above-footer-wrap[data-section="section-above-footer-builder"] .ast-builder-grid-row',
 			property: 'grid-column-gap',
-		} ).cssValueToBe( `${ innercolumnSpacing[ 'hba-inner-spacing' ].tablet }${ innercolumnSpacing[ 'hba-inner-spacing' ][ 'tablet-unit' ] }`,
-		);
+		} ).cssValueToBe( `${ innerColumnSpacing[ 'hba-inner-spacing' ].tablet }${ innerColumnSpacing[ 'hba-inner-spacing' ][ 'tablet-unit' ] }` );
 
 		await setBrowserViewport( 'small' );
 		await scrollToElement( '#colophon' );
 		await expect( {
 			selector: '.site-above-footer-wrap[data-section="section-above-footer-builder"] .ast-builder-grid-row',
 			property: 'grid-column-gap',
-		} ).cssValueToBe( `${ innercolumnSpacing[ 'hba-inner-spacing' ].mobile }${ innercolumnSpacing[ 'hba-inner-spacing' ][ 'mobile-unit' ] }`,
-		);
+		} ).cssValueToBe( `${ innerColumnSpacing[ 'hba-inner-spacing' ].mobile }${ innerColumnSpacing[ 'hba-inner-spacing' ][ 'mobile-unit' ] }` );
 	} );
 } );
