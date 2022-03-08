4ed2264face334955db8fffd28a12b8f877aa354
Simran <simranc@bsf.io>
2022-03-08 08:03:04 +0530

    modified code

--- tests/e2e/specs/customizer/footer-builder/above-footer/border-size-color.test.js
+++ tests/e2e/specs/customizer/footer-builder/above-footer/border-size-color.test.js
@@ -5,8 +5,8 @@ import { scrollToElement } from '../../../../utils/scroll-to-element';
 describe( 'Above footer border size and color setting in customizer', () => {
 	it( 'top border size and color should apply correctly', async () => {
 		const borderSizeColor = {
-			'hba-footer-separator': '50',
-			'hba-footer-top-border-color': 'rgb(255, 79, 88)',
+			'hba-footer-separator': '55',
+			'hba-footer-top-border-color': 'rgb(203, 237, 170)',
 			'footer-desktop-items': {
 				above: {
 					above_1: {
@@ -19,20 +19,16 @@ describe( 'Above footer border size and color setting in customizer', () => {
 		await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
-		await page.waitForSelector( '.site-above-footer-wrap[data-section="section-above-footer-builder"]' );
 		await setBrowserViewport( 'large' );
-
 		await scrollToElement( '#colophon' );
+		await page.waitForSelector( '.site-above-footer-wrap[data-section="section-above-footer-builder"]' );
 		await expect( {
 			selector: '.site-above-footer-wrap[data-section="section-above-footer-builder"]',
 			property: 'border-top-width',
-		} ).cssValueToBe( `${ borderSizeColor[ 'hba-footer-separator' ] + 'px' }`,
-		);
+		} ).cssValueToBe( `${ borderSizeColor[ 'hba-footer-separator' ] + 'px' }` );
 		await expect( {
 			selector: '.site-above-footer-wrap[data-section="section-above-footer-builder"]',
 			property: 'border-top-color',
-		} ).cssValueToBe( `${ borderSizeColor[ 'hba-footer-top-border-color' ] }`,
-
-		);
+		} ).cssValueToBe( `${ borderSizeColor[ 'hba-footer-top-border-color' ] }` );
 	} );
 } );
